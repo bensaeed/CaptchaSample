061f56ba83a46d644e2bcf59cf711c5fca1c7e41
mbensaeed <mbensaeed@bipc.org.ir>
2019-12-01 16:58:39 +0330

    Captcha Example

---
+++ WebAppCaptcha/WebAppCaptcha/Controllers/DefaultController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebAppCaptcha.Models;
+
+namespace WebAppCaptcha.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Result of CAPTCHA image displayed!";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string CaptchaText)
+        {
+            if (CaptchaText == HttpContext.Session["captchastring"].ToString())
+                ViewBag.Message = "CAPTCHA verification successful!";
+            else
+                ViewBag.Message = "CAPTCHA verification failed!";
+
+            return View();
+        }
+
+        public CaptchaImageResult ShowCaptchaImage()
+        {
+            return new CaptchaImageResult();
+        }
+    }
+}
---
+++ WebAppCaptcha/WebAppCaptcha/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAppCaptcha.MvcApplication" Language="C#" %>
